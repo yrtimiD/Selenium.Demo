ab2d90df1cf7f7ebc747a2f1522c0258afb93fa8
yrtimid <yrtimid@gmail.com>
2018-06-26 18:25:21 +0300

    initial commit

---
+++ Selenium.Chrome.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Chrome.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+			driver.Navigate().GoToUrl("https://http.cat/200");
+		}
+	}
+}
---
+++ Selenium.IE.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.IE.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var driver = new OpenQA.Selenium.IE.InternetExplorerDriver();
+			driver.Navigate().GoToUrl("https://http.cat/200");
+		}
+	}
+}
